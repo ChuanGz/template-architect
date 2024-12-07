a30020e140e4987ec18b7f7f8ac62a0b5d537508
Kendy <thanhnhan.lionus@gmail.com>
2024-12-07 12:56:31 +0700

    add fe pipeline

--- 2--Implementation/STU01/src/frontend/backoffice/src/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: `<h1>Backoffice Management</h1>`,
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {}
