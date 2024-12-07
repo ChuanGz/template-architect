c11a0bedf281ca81df4122b37c4426c97cfa7fbc
Kendy <thanhnhan.lionus@gmail.com>
2024-12-07 10:56:05 +0700

    update

--- 2--Implementation/STU01/src/services/order-service/Program.cs
+++ 2--Implementation/STU01/src/services/order-service/Program.cs
@@ -1,6 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace OrderServiceApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
 
-app.MapGet("/", () => "Order Service is running...");
+            app.MapGet("/", () => "Order Service is running...");
 
-app.Run();
+            app.Run();
+        }
+    }
+}
